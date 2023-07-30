de66b3602145beb53a05692dae6545024014380e
gkwk <124476558+gkwk@users.noreply.github.com>
2023-07-30 20:06:48 +0900

    boards 게시판 글 D 구현

--- ToyProject/boards/urls.py
+++ ToyProject/boards/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', boards.views.boards_index, name='index'),
     path('posting/', boards.views.boards_posting, name='posting'),
     path('notice/<int:pk>/', boards.views.boards_post_detail, name='post_detail'),
+    path('post_delete/<int:post_pk>/', boards.views.boards_delete_post, name='delete_post'),
     path('comment_delete/<int:comment_pk>/', boards.views.boards_delete_comment, name='delete_comment'),
 ]
 
--- ToyProject/boards/views.py
+++ ToyProject/boards/views.py
@@ -56,16 +56,6 @@ def boards_post_detail(request,pk):
             if new_CommentForm.is_valid():
                 new_CommentForm.save()
                 return redirect("boards:post_detail", pk=pk)
-    #         else:
-    #             new_CommentForm = CommentForm()
-    #             return render(request, 'notice_detail.html', {'post': post,"comment_list" : post_comment_list,"comment_form" : new_CommentForm, "post_pk" : pk})
-            
-    #     else:
-    #         new_CommentForm = CommentForm()
-    #         return render(request, 'notice_detail.html', {'post': post,"comment_list" : post_comment_list,"comment_form" : new_CommentForm, "post_pk" : pk})
-        
-    # else:
-    #     return render(request, 'notice_detail.html', {'post': post, "comment_list" : post_comment_list,})
     
     new_CommentForm = CommentForm()
     return render(request, 'boards/post_detail.html', {'post': post,"comment_list" : post_comment_list,"comment_form" : new_CommentForm, "post_pk" : pk})
@@ -74,80 +64,26 @@ def boards_delete_comment(request,comment_pk):
     if request.user.is_authenticated:
         if request.method == "POST":
             try:
-                notice_comment = Comment.objects.get(id=comment_pk) #쿼리셋을 얻는다.
-                if notice_comment.user_id_id == request.user.id: # *_id : db의 fk의 raw value에 접근한다.
-                    if notice_comment.user_id_char == request.user.username:
-                        reverse_notice_pk = notice_comment.post_id_id
-                        notice_comment.delete()
-                        return redirect("boards:post_detail", pk=reverse_notice_pk) 
+                post_comment = Comment.objects.get(id=comment_pk) #쿼리셋을 얻는다.
+                if post_comment.user_id_id == request.user.id: # *_id : db의 fk의 raw value에 접근한다.
+                    # if post_comment.user_id_char == request.user.username:
+                    reverse_post_pk = post_comment.post_id_id
+                    post_comment.delete()
+                    return redirect("boards:post_detail", pk=reverse_post_pk) 
             except:
                 pass
             
     return redirect("/")
 
-
-
-
-# def new_posting(request):
-#     # if request.user.is_authenticated:
-#     if request.user.is_superuser:
-#         if request.method == "GET":
-#             return render(request, 'new_posting.html')
-        
-#         elif request.method == "POST":
-#             new_post = Notice_post()
-#             new_post.uploader_id = request.user.username
-#             new_post.text = request.POST.get('editorTxt')
-#             new_post.title = request.POST.get('title')
+def boards_delete_post(request,post_pk):
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            try:
+                post = Post.objects.get(id=post_pk) #쿼리셋을 얻는다.
+                if post.user_id_id == request.user.id: # *_id : db의 fk의 raw value에 접근한다.
+                    post.delete()
+                    return redirect("boards:index")
+            except:
+                pass
             
-#             up_time = timezone.datetime.now()
-#             new_post.upload_date = up_time
-#             new_post.date_text = datetime.date.strftime(up_time, '%Y년 %m월 %d일 %H:%M:%S') 
-#             new_post.save()
-#             return redirect('notice:notice')
-
-#         return render(request, 'new_posting.html')
-#     else:
-#         return redirect('/')
-    
-# def notice_detail(request,pk):
-#     post = get_object_or_404(Notice_post, pk=pk)
-    
-#     notice_comment_list = Comment.objects.filter(post_id=pk).values("id","user_id","user_id_char","comment_body","created_date","updated_date") #쿼리셋을 얻는다.
-#     notice_comment_list = list(notice_comment_list) #쿼리셋을 리스트화한다.
-        
-#     if request.user.is_authenticated:
-#         if request.method == "POST":
-#             new_CommentForm = CommentForm(request.POST)
-                        
-#             if new_CommentForm.is_valid():
-
-#                 new_CommentForm = CommentForm({"post_id" : pk, "user_id" : request.user.id, "user_id_char" : request.user.username ,"comment_body" : new_CommentForm.cleaned_data["comment_body"]})
-#                 new_CommentForm.is_valid()
-#                 new_CommentForm.save()
-#                 return redirect("notice:notice_detail", pk=pk)
-#             else:
-#                 new_CommentForm = CommentForm()
-#                 return render(request, 'notice_detail.html', {'post': post,"comment_list" : notice_comment_list,"comment_form" : new_CommentForm, "post_pk" : pk})
-            
-#         else:
-#             new_CommentForm = CommentForm()
-#             return render(request, 'notice_detail.html', {'post': post,"comment_list" : notice_comment_list,"comment_form" : new_CommentForm, "post_pk" : pk})
-        
-#     else:
-#         return render(request, 'notice_detail.html', {'post': post, "comment_list" : notice_comment_list,})
-        
-# def notice_comment_delete(request,comment_pk):
-#     if request.user.is_authenticated:
-#         if request.method == "POST":
-#             try:
-#                 notice_comment = Comment.objects.get(id=comment_pk) #쿼리셋을 얻는다.
-#                 if notice_comment.user_id_id == request.user.id: # *_id : db의 fk의 raw value에 접근한다.
-#                     if notice_comment.user_id_char == request.user.username:
-#                         reverse_notice_pk = notice_comment.post_id_id
-#                         notice_comment.delete()
-#                         return redirect("notice:notice_detail", pk=reverse_notice_pk) 
-#             except:
-#                 pass
-            
-#     return redirect("/")
+    return redirect("/")
